6454c9460cb8a075cd61e2ad7cdb86605278bb02
jaykop <jaykop.jy@gmail.com>
2019-12-25 20:00:51 -0800

    Updates

---
+++ precache-manifest.4b5991a1c0a8eeea16406328b67189bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4821f0f7daf12890f2689e8ba72a1725",
+    "url": "/deploying_movie_app/index.html"
+  },
+  {
+    "revision": "a60ed06f427b258758d1",
+    "url": "/deploying_movie_app/static/css/main.5b380b7b.chunk.css"
+  },
+  {
+    "revision": "83216525801fc12a81ad",
+    "url": "/deploying_movie_app/static/js/2.2556ce37.chunk.js"
+  },
+  {
+    "revision": "0861ad41200203db0819c1c2e5edb5dd",
+    "url": "/deploying_movie_app/static/js/2.2556ce37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a60ed06f427b258758d1",
+    "url": "/deploying_movie_app/static/js/main.aee3613c.chunk.js"
+  },
+  {
+    "revision": "ffec0f52afe7d6bcb034",
+    "url": "/deploying_movie_app/static/js/runtime-main.56913b8a.js"
+  }
+]);
